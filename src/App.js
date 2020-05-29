693139c9058fe5970ee807b09ee9609d4c1e69ec
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-28 21:02:00 -0400

    created a nav with a sign out button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from './components/Nav/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <p>Sign Out</p>
+    </nav>
+  )
+}
+
+export default Nav;
