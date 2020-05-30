874f0c6d22a45a5a6803df18b5bd9c574b47af71
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-30 17:57:28 -0400

    removed the alt message and adjusted the width

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className='center ma4 w-50'>
-      <img src={imageUrl} alt='import' />
+    <div className='center ma4 w-60'>
+      <img src={imageUrl} alt='' />
     </div>
   )
 }
