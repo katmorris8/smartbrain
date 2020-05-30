4fd1bb5281c9b2883a985a832e46f1539258b9c5
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-29 21:16:16 -0400

    imported the rank and installed particles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React from 'react';
 import Nav from './components/Nav/Nav'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
 import './App.css';
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles
+        params={particlesOptions}
+      />
       <Nav />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
