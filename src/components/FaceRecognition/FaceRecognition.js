0a3b2ccfebe67b92efe9924e0b43a22a217f2ba7
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-30 17:37:53 -0400

    updated the image source to the image url prop

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className='center ma4'>
-      <img src={'https://samples.clarifai.com/face-det.jpg'} alt='import' />
+    <div className='center ma4 w-50'>
+      <img src={imageUrl} alt='import' />
     </div>
   )
 }
