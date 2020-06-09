fb9e8710a013975f256cfc899d87e69afa01faca
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-08 20:58:17 -0400

    added a conditional to on change route to update the state if the user is signed in or not. used destructuring to clean up the jsx

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -69,28 +70,34 @@ class App extends Component {
   }
 
   onRouteChange = route => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
     this.setState({ route: route });
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
           params={particlesOptions}
         />
-        { this.state.route === 'home' 
+        <Nav isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home' 
           ? <div>
-              <Nav onRouteChange={this.onRouteChange} />
               <Logo />
               <Rank />
               <ImageLinkForm
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
                 />
-              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
             : (
-              this.state.route === 'signin'
+              route === 'signin'
               ? <SignIn onRouteChange={this.onRouteChange} />
               : <Register onRouteChange={this.onRouteChange} />
             )
