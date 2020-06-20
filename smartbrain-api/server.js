899af55425fbd3b8b913b6d345c11f344c50afda
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-20 17:29:46 -0400

    created a list of routes and a test route

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('this is working');
+})
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 })
+
+/*
+/ --> res = this is working
+/signin --> POST req, res = success/fail
+/register --> POST req, res = user
+/profile/:userId --> GET req, res = user
+/image --> PUT req, res = user
+*/
