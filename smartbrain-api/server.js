27a8a7ef564084c4f9dcc3bb1f6bf9b64dcf2d25
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-20 18:07:56 -0400

    created an image route that tracks the numnber of image entries for users

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -64,6 +64,21 @@ app.get('/profile/:id', (req, res) => {
   }
 })
 
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(404).json('no such user');
+  }
+})
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 })
