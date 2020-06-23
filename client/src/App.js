3a69e6bc8eded9efe6ba75bdce9478a554d89960
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-22 21:48:09 -0400

    removed componentdidmount hook

--- client/src/App.js
+++ client/src/App.js
@@ -39,12 +39,6 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3000/')
-      .then(response => response.json())
-      .then(console.log)
-  }
-
   calculateFaceLocation = data => {
     const clarifyFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.querySelector('#input-image');
