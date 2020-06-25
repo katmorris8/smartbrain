f49a157abc93f2f5bcb51171b052063ad9731a69
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-25 17:46:09 -0400

    added the event listeners to the form for name, email and password

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -38,15 +38,33 @@ class Register extends Component {
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="name" name="name" id="name" />
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="name" 
+                  name="name" 
+                  id="name" 
+                  onChange={this.onNameChange}
+                />
               </div>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="email" 
+                  name="email-address" 
+                  id="email-address" 
+                  onChange={this.onEmailChange}
+                />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+                <input 
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="password" 
+                  name="password" 
+                  id="password" 
+                  onChange={this.onPasswordChange}
+                />
               </div>
             </fieldset>
             <div className="">
