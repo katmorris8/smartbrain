1be570f9babf4ce313d3c5931b420f4331f2dfee
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-30 21:01:08 -0400

    updated the user profile route with knex to retrieve the user's info from the database

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -79,16 +79,26 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  })
-  if (!found) {
-    res.status(404).json('no such user');
-  }
+  // let found = false;
+  // database.users.forEach(user => {
+  //   if (user.id === id) {
+  //     found = true;
+  //     return res.json(user);
+  //   }
+  // })
+  db.select('*').from('users').where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('User not found')
+      }
+    })
+    .catch(user => res.status(400).json('Error getting user'))
+
+  // if (!found) {
+  //   res.status(404).json('no such user');
+  // }
 })
 
 app.put('/image', (req, res) => {
