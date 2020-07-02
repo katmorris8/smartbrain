66e23c1958dc49796009b9698ee25ad8de4f8222
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-07-02 15:28:19 -0400

    removed the placeholder database

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -18,27 +18,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'john',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
   res.send(database.users);
 })
