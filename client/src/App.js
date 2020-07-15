2a607d23ec8f3587292c03adf4e66a727491e0ee
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-07-15 18:53:16 -0400

    removed the clarifai call to move it to the backend

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,6 @@ import Register from './components/Register/Register';
 import './App.css';
 import 'tachyons';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
-
-const app = new Clarifai.App({
-  apiKey: '7ea32a0e89af4468af9072d16d5fb26a'
-});
 
 const particlesOptions = {
   particles: {
@@ -83,9 +78,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
+    
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
