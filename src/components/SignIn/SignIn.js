2df552ce0068b654a1939c004498ba18453b2654
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-07-30 18:17:12 -0400

    updated fetch urls

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -23,7 +23,7 @@ class SignIn extends Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://powerful-waters-52296.herokuapp.com//signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
